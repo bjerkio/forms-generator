cc75347cf33f0e052647a74cebdd62cd2bd621e8
Simen A. W. Olsen <so@bjerk.io>
2022-02-07 15:49:37 +0100

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,59 @@
+import type { AppProps } from 'next/app';
+import { ThemeProvider, Container, merge } from 'theme-ui';
+import { theme } from '@bjerk/brand';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import Head from 'next/head';
+
+const policyTheme = merge(theme, {
+  styles: {
+    root: {
+      backgroundColor: 'white',
+    },
+    h1: {
+      fontSize: 3,
+      mb: 5,
+    },
+    h2: {
+      fontSize: 2,
+      fontWeight: 'bold',
+    },
+  },
+});
+
+function App({ Component, pageProps }: AppProps) {
+  const [dataProps, setDataProps] = useState({});
+  const router = useRouter();
+
+  useEffect(() => {
+    if (router.query.data) {
+      const dataFromBase64 = Buffer.from(
+        router.query.data.toString(),
+        'base64',
+      ).toString('utf-8');
+      setDataProps(JSON.parse(dataFromBase64));
+    }
+  }, [setDataProps, router.query]);
+
+  return (
+    <ThemeProvider theme={policyTheme}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Container sx={{ width: 'readable', p: 4 }}>
+        <Component {...pageProps} {...dataProps} />
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
